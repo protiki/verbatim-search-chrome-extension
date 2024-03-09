465fab82560f5a1702ff12ea509740fddd35e3ff
protiki <77539344+protiki@users.noreply.github.com>
2024-03-09 20:22:20 +0100

    customize bookmarklet-code.js with verbatim search function

--- bookmarklet-code.js
+++ bookmarklet-code.js
@@ -1,7 +1,9 @@
 (function(){
 	"use strict";
 
-	alert("A am a bookmarklet");
-	//add your code here;
+	let url = window.location.href;
+	if (url.indexOf('google.com/search') !== -1 && url.indexOf('&tbs=li:1') === -1) 
+		{ window.location.href = url + '&tbs=li:1';
+		} 
 
 })();
